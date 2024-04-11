9aa7d81bea521bdcb50229847071d5ba6e2ae07b
haaldeer26@gmail.com <halderflex26@gmail.com>
2024-04-11 20:56:34 +0100

    Proyecto Creado y dirección de la base de datos añadida.

---
+++ src/test/java/org/backend/bloghalderbackend/BlogHalderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.bloghalderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogHalderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
