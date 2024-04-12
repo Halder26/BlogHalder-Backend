9807c76773c822f4c8ae29817a7a3dac96b97a74
haaldeer26@gmail.com <halderflex26@gmail.com>
2024-04-12 12:43:03 +0100

    Anotación faltante en el repositorio y usuario en el archivo de propiedades.

--- src/main/java/org/backend/bloghalderbackend/Repositories/UserRepository.java
+++ src/main/java/org/backend/bloghalderbackend/Repositories/UserRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.backend.bloghalderbackend.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findByEmail(String email);
     boolean existsUserByEmail(String mail);
