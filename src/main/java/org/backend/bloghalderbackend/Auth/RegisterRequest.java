66c4f4cefbb1dc843dfaa1653ae767481bed6d6c
haaldeer26@gmail.com <halderflex26@gmail.com>
2024-04-12 12:03:05 +0100

    Respuesta con token y Request de login y register creado.

---
+++ src/main/java/org/backend/bloghalderbackend/Auth/RegisterRequest.java
@@ -0,0 +1,17 @@
+package org.backend.bloghalderbackend.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    String email;
+    String password;
+    String name;
+    String country;
+}
