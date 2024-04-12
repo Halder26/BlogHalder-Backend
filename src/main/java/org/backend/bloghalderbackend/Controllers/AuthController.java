f07992d0fc1059f983649a52d2024a4dadd3ef35
haaldeer26@gmail.com <halderflex26@gmail.com>
2024-04-12 12:28:45 +0100

    Controlador de autenticación creado con login y register.

---
+++ src/main/java/org/backend/bloghalderbackend/Controllers/AuthController.java
@@ -0,0 +1,31 @@
+package org.backend.bloghalderbackend.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.backend.bloghalderbackend.Services.AuthService;
+import org.backend.bloghalderbackend.Auth.AuthResponse;
+import org.backend.bloghalderbackend.Auth.LoginRequest;
+import org.backend.bloghalderbackend.Auth.RegisterRequest;
+@RestController
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
+    {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
+    {
+        return ResponseEntity.ok(authService.register(request));
+    }
+}
